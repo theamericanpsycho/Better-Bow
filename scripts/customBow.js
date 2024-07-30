a3425096dc84dae5c05490648038bc48edf66a37
BatemanCodes <patrickbatemandeveloper@gmail.com>
2024-07-30 15:39:13 +0800

    ???
    poor old better bow

--- scripts/customBow.js
+++ scripts/customBow.js
@@ -29,11 +29,33 @@
 
 import { EntityComponentTypes, world } from '@minecraft/server';
 
-function displayEntityName(typeId) {
-    let entityName = typeId.replace('minecraft:', '');
-    entityName = entityName.replace('_v2', '');
-    entityName = entityName.charAt(0).toUpperCase() + entityName.slice(1);
-    return entityName;
+const entityNameMap = {
+    'ender_dragon': 'Ender Dragon',
+    'piglin_brute': 'Piglin Brute',
+    'zombie_pigman': 'Zombie Pigman',
+    'evocation_illager': 'Evoker',
+    'polar_bear': 'Polar Bear',
+    'skeleton_horse': 'Skeleton Horse',
+    'zombie_horse': 'Zombie Horse',
+    'wither_skeleton': 'Wither Skeleton',
+    'cave_spider': 'Cave Spider',
+    'elder_guardian': 'Elder Guardian',
+    'iron_golem': 'Iron Golem',
+    'magma_cube': 'Magma Cube',
+    'snow_golem': 'Snow Golem',
+    'zombie_villager': 'Zombie Villager',
+    'wandering_trader': 'Wandering Trader',
+    'glow_squid': 'Glow Squid',
+    'trader_llama': 'Trader Llama'
+};
+
+function capitalizeWords(str) {
+    return str.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+}
+
+function formatEntityName(name) {
+    name = name.replace('_v2', '').toLowerCase();
+    return entityNameMap[name] || capitalizeWords(name.replace(/_/g, ' '));
 }
 
 world.afterEvents.projectileHitEntity.subscribe((data) => {
@@ -41,33 +63,17 @@ world.afterEvents.projectileHitEntity.subscribe((data) => {
     const victim = data.getEntityHit().entity;
     const health = victim?.getComponent(EntityComponentTypes.Health);
     const mainhandItem = shooter?.getComponent('minecraft:equippable')?.getEquipment('Mainhand');
+
     if (shooter?.typeId !== "minecraft:player")
         return;
+
     if (shooter?.typeId === "minecraft:player" && mainhandItem?.typeId === "minecraft:bow") {
         shooter?.runCommandAsync(`playsound random.orb @s`);
+
         if (victim.typeId === "minecraft:player") {
             shooter?.sendMessage(`§9${victim.name}'s HP: §c${health.currentValue.toFixed(2)}`);
         } else {
-            let victimName = victim.nameTag ? victim.nameTag : displayEntityName(victim.typeId);
-            victimName = victimName.replace('_v2', '');
-            victimName = victimName.replace('Ender_dragon', 'Ender Dragon');
-            victimName = victimName.replace('Piglin_brute', 'Piglin Brute');
-            victimName = victimName.replace('Zombie_pigman', 'Zombie Pigman');
-            victimName = victimName.replace('Evocation_illager', 'Evoker');
-            victimName = victimName.replace('Polar_bear', 'Polar Bear');
-            victimName = victimName.replace('Skeleton_horse', 'Skeleton Horse');
-            victimName = victimName.replace('Zombie_horse', 'Zombie Horse');
-            victimName = victimName.replace('Wither_skeleton', 'Wither Skeleton');
-            victimName = victimName.replace('Cave_spider', 'Cave Spider');
-            victimName = victimName.replace('Elder_guardian', 'Elder Guardian');
-            victimName = victimName.replace('Iron_golem', 'Iron Golem');
-            victimName = victimName.replace('Magma_cube', 'Magma Cube');
-            victimName = victimName.replace('Snow_golem', 'Snow Golem');
-            victimName = victimName.replace('Zombie_villager', 'Zombie Villager');
-            victimName = victimName.replace('Wandering_trader', 'Wandering Trader');
-            victimName = victimName.replace('Glow_squid', 'Glow Squid');
-            victimName = victimName.replace('Trader_llama', 'Trade Llama');
-
+            let victimName = victim.nameTag || formatEntityName(victim.typeId.replace('minecraft:', ''));
             if (health.currentValue > 0) {
                 shooter?.sendMessage(`§9${victimName}'s HP: §c${health.currentValue.toFixed(2)}`);
             }
