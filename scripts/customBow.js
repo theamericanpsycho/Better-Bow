a93619ee2b7701478b7ce6676a7a8fafad750b82
BatemanCodes <patrickbatemandeveloper@gmail.com>
2024-08-11 20:59:51 +0800

    updated customBow.js

--- scripts/customBow.js
+++ scripts/customBow.js
@@ -27,41 +27,21 @@
 * Discord: @itstheamericanpsycho
 */
 
-import { EntityComponentTypes, world } from '@minecraft/server';
-
-const entityNameMap = {
-    'ender_dragon': 'Ender Dragon',
-    'piglin_brute': 'Piglin Brute',
-    'zombie_pigman': 'Zombie Pigman',
-    'evocation_illager': 'Evoker',
-    'polar_bear': 'Polar Bear',
-    'skeleton_horse': 'Skeleton Horse',
-    'zombie_horse': 'Zombie Horse',
-    'wither_skeleton': 'Wither Skeleton',
-    'cave_spider': 'Cave Spider',
-    'elder_guardian': 'Elder Guardian',
-    'iron_golem': 'Iron Golem',
-    'magma_cube': 'Magma Cube',
-    'snow_golem': 'Snow Golem',
-    'zombie_villager': 'Zombie Villager',
-    'wandering_trader': 'Wandering Trader',
-    'glow_squid': 'Glow Squid',
-    'trader_llama': 'Trader Llama'
-};
+import * as bc from '@minecraft/server';
 
 function capitalizeWords(str) {
     return str.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
 }
 
 function formatEntityName(name) {
-    name = name.replace('_v2', '').toLowerCase();
-    return entityNameMap[name] || capitalizeWords(name.replace(/_/g, ' '));
+    name = name.replace('minecraft:', '').replace('_v2', '').replace(/_/g, ' ');
+    return capitalizeWords(name);
 }
 
-world.afterEvents.projectileHitEntity.subscribe((data) => {
+bc.world.afterEvents.projectileHitEntity.subscribe((data) => {
     const shooter = data.source;
     const victim = data.getEntityHit().entity;
-    const health = victim?.getComponent(EntityComponentTypes.Health);
+    const health = victim?.getComponent(bc.EntityComponentTypes.Health);
     const mainhandItem = shooter?.getComponent('minecraft:equippable')?.getEquipment('Mainhand');
 
     if (shooter?.typeId !== "minecraft:player")
@@ -73,7 +53,7 @@ world.afterEvents.projectileHitEntity.subscribe((data) => {
         if (victim.typeId === "minecraft:player") {
             shooter?.sendMessage(`§9${victim.name}'s HP: §c${health.currentValue.toFixed(2)}`);
         } else {
-            let victimName = victim.nameTag || formatEntityName(victim.typeId.replace('minecraft:', ''));
+            let victimName = victim.nameTag || formatEntityName(victim.typeId);
             if (health.currentValue > 0) {
                 shooter?.sendMessage(`§9${victimName}'s HP: §c${health.currentValue.toFixed(2)}`);
             }
